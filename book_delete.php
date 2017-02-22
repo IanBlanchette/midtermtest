a19f3d7d8d3e1ad4a3aacee737c3435b41266635
Ian <ianjblanchette@gmail.com>
2017-02-22 15:31:37 -0500

    index page update

--- book_delete.php
+++ book_delete.php
@@ -4,7 +4,13 @@ include_once('database.php');
 /*//////////////////////*/
 /* YOUR CODE GOES HERE */
 /*/////////////////////*/
-
+$book = $_GET['Id'];
+if($book != false)
+	$query = "DELETE FROM books WHERE id = :id";
+	$statement = $db->prepare($query);
+	$statement->bindValue(":id", $book);
+	$success = $statement->execute();
+	$statement->closeCursor();
 // redirect to index page
 header('Location: index.php');
 
