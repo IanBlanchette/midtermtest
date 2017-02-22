e7a8699335841f8bfe01bed114705e3db0b70c7c
Ian <ianjblanchette@gmail.com>
2017-02-22 16:35:20 -0500

    delete page update

--- book_delete.php
+++ book_delete.php
@@ -4,14 +4,13 @@ include_once('database.php');
 /*//////////////////////*/
 /* YOUR CODE GOES HERE */
 /*/////////////////////*/
-$book = $_GET['BookID'];
-if($book != false)
-	$query = "DELETE FROM books WHERE id = :id";
-	$statement = $db->prepare($query);
-	$statement->bindValue(":id", $book);
-	$success = $statement->execute();
-	$statement->closeCursor();
+$bookID = $_GET["bookID"]; // assigns the gameID from the URL
+if($bookID != false) {
+    $query = "DELETE FROM book WHERE Id = :book_id ";
+    $statement = $db->prepare($query);
+    $statement->bindValue(":book_id", $bookID);
+    $success = $statement->execute(); // execute the prepared query
+    $statement->closeCursor(); // close off database
+}
 // redirect to index page
 header('Location: index.php');
-
-?>
